ab6a8af423d7f63cb3f0bf928e444e75758874ad
gonza <ngonzalosuarez@gmail.com>
2022-09-16 14:48:46 -0300

    Fixes

--- WEB_API/Program.cs
+++
@@ -1,7 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.UseSwagger();
-app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
-
-app.Run();
